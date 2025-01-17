426001a393155920b65caa34ea449f2ea7f49147
Mikayel Aghumyan <maghumya@c2r4s2.42yerevan.am>
2025-01-17 17:16:47 +0400

    implement %s

--- ft_printf.c
+++ ft_printf.c
@@ -1,10 +1,10 @@
 #include "./ft_printf.h"
-#include "./libft/libft.h"
 #include <stdio.h>
 
-int	ft_determinant(const char *format)
+int	ft_determinant(const char *format, va_list args)
 {
 	int	bytes;
+	char *str;
 
 	bytes = 0;
 	if (*format == '%')
@@ -12,32 +12,41 @@ int	ft_determinant(const char *format)
 		ft_putchar_fd('%', 1);
 		bytes++;
 	}
+	else if (*format == 's') {
+		str = va_arg(args, char *);
+		ft_putstr_fd(str,1);
+		bytes += ft_strlen(str);
+	}
 	return (bytes);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	unsigned int	bytes;
+	va_list	args;
 
+	va_start(args, format);
 	bytes = 0;
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			bytes += ft_determinant(format++);
+			bytes += ft_determinant(format++, args);
 			continue ;
 		}
 		ft_putchar_fd(*format, 1);
 		bytes++;
 		format++;
 	}
+	va_end(args);
 	return (bytes);
 }
 
 int	main(int argc, char **argv)
 {
 	(void)argc;
-	printf("\nbytes printed: %d", ft_printf(argv[1]));
+	char *str = "hello world";
+	printf("\nbytes printed: %d", ft_printf(argv[1], str));
 	return (0);
 }
